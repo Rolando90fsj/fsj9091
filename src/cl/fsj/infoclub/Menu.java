c717aba4f56d1925ce709f4d59ae6f8ba5bedc09
Rolando90fsj <110207607+Rolando90fsj@users.noreply.github.com>
2022-07-31 00:26:10 -0400

    update Menu

--- src/cl/fsj/infoclub/Menu.java
+++ src/cl/fsj/infoclub/Menu.java
@@ -5,57 +5,12 @@ import java.util.Scanner;
 public class Menu {
 
 	
-//	Scanner sc = new Scanner (System.in);
-//	ProductoServicio productoServicio = new ProductoServicio(); // listar productos
-//	ArchivoServicio archivoServicio = new ArchivoServicio();    // cargar datos
-//	
-	
 	public static void menu () {
-//		
-//		String op = "";
-//		
-//		do {
-//		
 		System.out.println("MENU PRINCIPAL");
 		System.out.println("1. Listar Producto");
 		System.out.println("2. Editar producto");
 		System.out.println("3. Importar datos");
 		System.out.println("4. Salir \n");
 		System.out.println("Elija una opcion");
-//		op= sc.nextLine();
-//		
-//		if(op.equalsIgnoreCase("1") || op.equalsIgnoreCase("2") || op.equalsIgnoreCase("3") || op.equalsIgnoreCase("4")) {
-//		int opcion = Integer.parseInt(op);
-//		switch (opcion) {
-//		case 1:
-//			productoServicio.getListaProductos();
-//		case 2:
-//		//	editarDatos();
-//		case 3:
-//			archivoServicio.cargarDatos();
-//		}
-//		
-//		}else {
-//			
-//		}
-//		
-//		
-//		}while (!op.equalsIgnoreCase("4")) ;
-//		
-}	
-//		public void editarDatos() {
-//			
-//			if(productoServicio.getListaProductos().size()==0) {
-//				System.out.println("Lista Vacia");
-//			}else {
-//				Scanner sc = new Scanner(System.in);
-//				System.out.println("Ingrese el codigo del producto: ");
-//				String codigo = sc.nextLine();
-//				
-//				for (Producto producto:productoServicio.getListaProductos()) {
-//					
-//				}
-//			}
-//		}
-			
+	}
 }
